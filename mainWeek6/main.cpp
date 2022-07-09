7976014abdcea6b00b1ce86e08001c0c3cec3de4
Tech&Beauty <thebioshive@gmail.com>
2022-07-09 15:17:38 -0400

    updates functions and variables for acceptData function

--- mainWeek6/main.cpp
+++ mainWeek6/main.cpp
@@ -22,49 +22,35 @@ int main()
     cout << "**** Points are given on a scale 1 -10, Enter divers scores below. ****\n"<<endl;
     acceptsData();
     cout << "********************************\n";
-    
-    determineLowestScore();
-    cout << "********************************\n";
-    
+
     determineHighestScore();
+    cout << "********************************\n";
+
 
 }
 
 //MARK: functions
 //Accept the data – prompt the user for the judge’s score. average score
 void acceptsData() {
-    double score1,
-    score2,
-    score3,
-    score4,
-    score5,
-    score6;
-    
-    cout << "Enter Judges Score: "<<endl;
-    cin >> score1;
-    
-    cout << "Enter Judges Score: "<<endl;
-    cin >> score2;
-    
-    cout << "Enter Judges Score: "<<endl;
-    cin >> score3;
-    
-    cout << "Enter Judges Score: "<<endl;
-    cin >> score4;
-    
-    cout << "Enter Judges Score: "<<endl;
-    cin >> score5;
-    
-    cout << "Enter Judges Score: "<<endl;
-    cin >> score6;
+    float judgeScore;
+
+    //dispaly 6 judges scores and only scoregrades 1-10 can be entered
+    for(int scores = 0; scores<= 9; scores++) {
+        cout<< "Enter Judges Score " << scores + 1 << " : " << endl;
+        cin >> judgeScore;
+    }
 }
 
 //determine highest score of all scores
 void determineHighestScore () {
    //variables
+  
 }
 
 //determine lowest score of all scores
 void determineLowestScore() {
     //variables
+    
 }
+
+
